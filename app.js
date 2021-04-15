6342f06394ca14115091664997cd1f493557d2b5
andrewsilent <silent.netix@gmail.com>
2021-04-15 19:40:41 +0300

    git init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const router = require('./routes');
+const errorHandler = require('./middlewares/errorHandler.mw');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api', router);
+app.use(errorHandler);
+
+module.exports = app;
